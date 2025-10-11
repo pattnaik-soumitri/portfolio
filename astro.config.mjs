3c16c627e269694b3e2faef3596c55b923aa5cc7
Soumitri Pattnaik <soumitri.pattnaik@gmail.com>
2025-10-12 04:02:10 +0530

    Added Varcel adaptor

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,18 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+
   vite: {
     plugins: [tailwindcss()],
     server: {
       allowedHosts: ['localhost']
     }
   },
+
+  adapter: vercel(),
 });
